a84ee747e86a355c777dd3895e26ab9913c12c99
michaelchen <jr.michael.chen@gmail.com>
2018-09-11 19:55:20 -0400

    Initial Commit

---
+++ FLL 2018-2019 Scorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FLL 2018-2019 Scorer
+//
+//  Created by Michael Chen on 9/11/18.
+//  Copyright © 2018 Michael Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
